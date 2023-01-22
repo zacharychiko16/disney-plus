10e662a09ce27ea7d34cfca110921b6f60101636
zacharychiko16 <zacharychiko16@gmail.com>
2023-01-22 18:26:50 -0400

    login page & navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <Header/>
         <Switch>
           <Route exact path='/'>
             <Login />
